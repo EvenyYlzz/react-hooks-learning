337ac188f520e9f71ef9730549235556cd872f48
EvenyYL <172463074@qq.com>
2020-11-21 17:28:25 +0800

    useState

--- src/components/StateClass.js
+++ src/components/StateClass.js
@@ -10,10 +10,16 @@ class StateClass extends React.Component{
 
     render() {
         return (
-            <div>
+            <div onClick={ this.setName }>
                 这是一个类组件————{ this.state.name }
             </div>
         )
+    }
+
+    setName = () => {
+        this.setState({
+            name: '我通过类组件方法变成这样了'
+        })
     }
 }
 
