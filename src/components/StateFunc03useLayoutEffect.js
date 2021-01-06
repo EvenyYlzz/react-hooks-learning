ffc9222fde2668ee22ffeac58d944438b9dc1d53
EvenyYlzz <172463074@qq.com>
2021-01-07 01:49:10 +0800

    错误或补充

--- src/components/StateFunc03useLayoutEffect.js
+++ src/components/StateFunc03useLayoutEffect.js
@@ -5,7 +5,7 @@ import React,{ useEffect, useState, useLayoutEffect } from 'react'
 //  相似的用法
 //  1.接收一个函数作为参数
 //  2.接收第二个参数  依赖列表  只有依赖更新时，才会执行函数
-//  3.返回一个函数，①限制性返回函数，②再执行参数函数
+//  3.返回一个函数，①先执行返回函数，②再执行参数函数
 function StateFunction () {
 
     const [num, setNum] = useState(0)
@@ -13,6 +13,7 @@ function StateFunction () {
     //不同点
     //useEffect 执行时机在 render 之后
     //useLayoutEffect dom更新之后
+    //在类组件中用componentWillMount生命周期来实现
     useLayoutEffect( () => {
         console.log('useLayoutEfffect')
 
