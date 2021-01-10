b4281cc8e22fac1e6c318f881605bafa5be62d74
EvenyYlzz <172463074@qq.com>
2021-01-10 17:06:41 +0800

    useCallback补充完整

--- src/components/StateFunc05useCallback.js
+++ src/components/StateFunc05useCallback.js
@@ -1,12 +1,13 @@
-import { render } from '@testing-library/react'
-import React,{ useEffect, useState, useLayoutEffect, useMemo, useCallback } from 'react'
+import { useEffect, useState, useLayoutEffect, useMemo, useCallback } from 'react'
 
 const set = new Set()
 
 function StateFunction () {
 
-    const [num, setNum] = useState(0)
+    const [num, setNum] = useState(1)
     const [age, setAge] = useState(18)
+
+    //  useMemo( () => fn,deps ) 相当于 useCallBack(fn,deps)
 
     //  在使用方法上，useCallback 与 useMemo 相同
     //  useMemo返回的是一个值
@@ -15,13 +16,20 @@ function StateFunction () {
     //  1.useMemo返回的是一个值，useCallback返回的是一个函数
     //  2.useMemo缓存的是一个值，useCallback缓存的是一个函数
     const getDoubleNum = useCallback( () => {
-        console.log('useCallback')
+        // console.log('useCallback')
+        console.log(`获取双倍Num${num}`)
         return 2 * num
     },[num] )
 
+    // const getDoubleNum = useMemo( () => {
+    //     console.log(`获取双倍Num${num}`)
+    //     return 2 * num
+    // },[] )
+
     //  设置依赖为[]，[num]进行一个对比，会发现set的长度的两种变化
-    set.add(getDoubleNum)
+    set.add(getDoubleNum())
     console.log('set.size：',set.size)
+    // console.log('set.size：',set.size)
     
     // function getDoubleNum () {
     //     console.log(`获取双倍Num${num}`)
@@ -30,11 +38,14 @@ function StateFunction () {
     // }
 
     return (
-        <div onClick={ () => setNum( num => num+1 ) }>
+        // <div onClick={ () => {setNum( num => num+1 )} }>
+        <div onClick={ () => {setAge( age => age+1 )} }>
             这是一个函数式组件————num:{  getDoubleNum() }
             <br></br>
             age的值为————age:{ age }
-            <Child callback={ getDoubleNum }></Child>
+            <br></br>
+            set.size:{set.size}
+            <Child callback={ getDoubleNum() }></Child>
         </div>
     )
 }
@@ -42,7 +53,7 @@ function StateFunction () {
 
 //  使用场景，父组件的传入函数不更新，就不会触发子组件的函数
 function Child(props) {
-    console.log(props.callback)
+    console.log('子组件更新了')
 
     useEffect( () => {
         console.log('callback更新了')
@@ -50,7 +61,7 @@ function Child(props) {
 
     return (
         <div>
-            child
+            子组件的getDoubleNum{props.callback}
         </div>
     )
 }
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,7 @@
 import logo from '../images/logo.svg';
 import StateClass from '../components/StateClass'
 // import StateFunction from '../components/StateFunction'
-import StateFunction from '../components/StateFunc04useMemo'
+import StateFunction from '../components/StateFunc05useCallback'
 import './App.css';
 
 import {  useEffect, useState } from 'react'
@@ -9,22 +9,28 @@ import {  useEffect, useState } from 'react'
 function App() {
   const [bool,setBool] = useState(true)
 
-  if(bool){
-    return (
-      <div className="App">
-        <StateClass></StateClass>
-        <StateFunction></StateFunction>
-        <button onClick={ () => { setBool(!bool) } }>点击我</button>
-      </div>
-    );
-  }else{
-    return (
-      <div className="App">
-        <StateClass></StateClass>
-        <button onClick={ () => { setBool(!bool) } }>点击我</button>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <StateClass></StateClass>
+      <StateFunction></StateFunction>
+    </div>
+  );
+  // if(bool){
+  //   return (
+  //     <div className="App">
+  //       <StateClass></StateClass>
+  //       <StateFunction></StateFunction>
+  //       <button onClick={ () => { setBool(!bool) } }>点击我</button>
+  //     </div>
+  //   );
+  // }else{
+  //   return (
+  //     <div className="App">
+  //       <StateClass></StateClass>
+  //       <button onClick={ () => { setBool(!bool) } }>点击我</button>
+  //     </div>
+  //   );
+  // }
   
 }
 
